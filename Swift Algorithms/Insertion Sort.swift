96189829b54380988c734c43cfc6720defce69b2
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2023-11-01 17:12:14 +0000

    Tried to figure out how to do insertion sort.

--- Swift Algorithms/Insertion Sort.swift
+++ Swift Algorithms/Insertion Sort.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-func InsertionSort -> [Int] {
-    
+func insertion_sort(Number_Array: inout [Int]) {
+    for i in 1...Number_Array.count - 1 {
+        if Number_Array[i] > Number_Array[i-1] {
+            var temp = Number_Array[i-1]
+            Number_Array[i-1] = Number_Array[i]
+            Number_Array[i] = temp
+        }
+    }
 }
+
+
+
--- Swift Algorithms/main.swift
+++ Swift Algorithms/main.swift
@@ -1,3 +1,17 @@
 import Foundation
 
-var data = [54,26,32,51,56,31,76,83,95]
+var Number_Array = [Int]()
+
+func array_generator(array: inout [Int]) {
+    for _ in 1...10 {
+        array.append(Int.random(in: 1...100))
+    }
+}
+
+array_generator(array: &Number_Array)
+
+
+print(Number_Array)
+
+
+insertion_sort(Number_Array: &Number_Array)
