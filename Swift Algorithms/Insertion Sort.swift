a83d3424803bed78021e9e5764ce03c58bb65d19
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2023-11-03 11:20:50 +0000

    Working on Quick Sort.

--- Swift Algorithms/Insertion Sort.swift
+++ Swift Algorithms/Insertion Sort.swift
@@ -1,14 +1,14 @@
-import Foundation
-
-func insertion_sort(Number_Array: inout [Int]) {
-    for i in 1...Number_Array.count - 1 {
-        if Number_Array[i] > Number_Array[i-1] {
-            var temp = Number_Array[i-1]
-            Number_Array[i-1] = Number_Array[i]
-            Number_Array[i] = temp
-        }
-    }
-}
-
-
+//import Foundation
+//
+//func insertion_sort(Number_Array: inout [Int]) {
+//    for i in 1...Number_Array.count - 1 {
+//        if Number_Array[i] > Number_Array[i-1] {
+//            var temp = Number_Array[i-1]
+//            Number_Array[i-1] = Number_Array[i]
+//            Number_Array[i] = temp
+//        }
+//    }
+//}
+//
+//
 
--- Swift Algorithms/main.swift
+++ Swift Algorithms/main.swift
@@ -1,17 +1,49 @@
 import Foundation
 
-var Number_Array = [Int]()
+//var Number_Array = [Int]()
+//
+//func array_generator(array: inout [Int]) {
+//    for _ in 1...10 {
+//        array.append(Int.random(in: 1...100))
+//    }
+//}
+//
+//array_generator(array: &Number_Array)
+//
+//
+//print(Number_Array)
+//
+//
+//insertion_sort(Number_Array: &Number_Array)
 
-func array_generator(array: inout [Int]) {
-    for _ in 1...10 {
-        array.append(Int.random(in: 1...100))
-    }
+var data = [25,78,12,34,54,22,83,45,87]
+
+func QuickSort(data: [Int]) -> [Int] {
+    let pivot = data[0]
+    var left = [Int]()
+    var right = [Int]()
+    if data.count > 1 {
+        for i in 1..<data.count {
+            if data[i] < pivot {
+                left.append(data[i])
+            }
+            else {
+                right.append(data[i])
+            }
+        }
+        
+    } else {
+        left = QuickSort(data: left)
+        right = QuickSort(data: right)
+        }
+    
+    var MergeArray = [Int]()
+    MergeArray.append(contentsOf: left)
+    MergeArray.append(pivot)
+    MergeArray.append(contentsOf: right)
+    
+    return MergeArray
 }
 
-array_generator(array: &Number_Array)
-
-
-print(Number_Array)
-
-
-insertion_sort(Number_Array: &Number_Array)
+let SortedArray = QuickSort(data: data)
+print(SortedArray)
