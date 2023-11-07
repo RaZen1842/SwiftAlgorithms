ad8455c2d84486072cbe808cce97d6d1a99dde8e
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2023-11-07 13:17:02 +0000

    Testing Algorithms

---
+++ Swift Algorithms/Algorithm.swift
@@ -0,0 +1,19 @@
+//
+//  Algorithm.swift
+//  Swift Algorithms
+//
+//  Created by Veturi, Shreyas (HJRM) on 07/11/2023.
+//
+
+import Foundation
+
+
+class Algorithm {
+    func linearSearch(data: [Int], item: Int) -> Bool {
+        
+        if data.count == 0 {
+            return false
+        }
+        return true
+    }
+}
