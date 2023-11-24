dbfd5588e0027fb21305ae4d37293a860ec8f353
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2023-11-24 11:20:08 +0000

    Finished Started Quick Sort

--- Swift Algorithms/Quick Sort.swift
+++ Swift Algorithms/Quick Sort.swift
@@ -1,7 +1,14 @@
 import Foundation
 
 class QuickSort {
-    func quickSort() -> [Int] {
+    func quickSort(data: [Int]) -> [Int] {
+        
+        if data.count <= 1 {
+            return data
+        }
+        
+        var pivot = data[0]
+        
         
     }
 }
